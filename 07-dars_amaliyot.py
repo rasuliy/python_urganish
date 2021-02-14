cbc80c7475abbb5780d8dbec0c401afc89693d86
Abdurasul Pirnazarov <abdurasul1981@gmail.com>
2021-02-14 21:45:34 +0500

    Create 07-dars_amaliyot.py

---
+++ 07-dars_amaliyot.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+ismlar = ['Ali', 'Vali', 'Dali']
+print(f'Salom {ismlar[0]}, bugun choyxona bormi?')
+print(ismlar[0] + ', ' + ismlar[1] + ', ' + ismlar[2] +' do\'stlar!')
+print(ismlar[-1].rstrip() +', yahshi yuribsanmi?')
+
+sonlar = [20, 34, 78, 2.56, 0, 34.1, -56, 9.0, 43, 89]
+print(sonlar[0]+sonlar[-1])
+print(sonlar)
+sonlar.append(3)
+print(sonlar)
+sonlar.remove(3)
+print(sonlar)
+sonlar.insert(2, -2)
+print(sonlar)
+del sonlar[2]
+print(sonlar)
+print(sonlar[2]/sonlar[0])
+print(sonlar[2]//sonlar[0])
+
+sonlar_2=list(range(0,10))
+print(sonlar_2)
+
+juft_sonlar = list(range(0,20,2))
+print(juft_sonlar)
+
+toq_sonlar = list(range(1,20,2))
+print(toq_sonlar)
+
+cars = ['bmw', 'mercedes bens', 'volvo', 'GM', 'tesla', 'audi']
+my_cars = cars[2:5]
+print(my_cars)
+
+mexmonlar = ['ali', 'vali', 'gani', 'dali']
+for mexmon in mexmonlar:
+    print(mexmon)
+
+sonlar_3 = list(rang(11))
+sonlar_kvadrati = []
+
